3bc54c5f1159edd6f1753229f99593a3193fb2c7
Kiza <belteksllc@gmail.com>
2019-02-18 08:38:34 +0100

    Izmene klasa

--- src/LaravelVuePagination.vue
+++ src/LaravelVuePagination.vue
@@ -1,8 +1,7 @@
 <template>
     <renderless-laravel-vue-pagination :data="data" :limit="limit" :show-disabled="showDisabled" v-on:pagination-change-page="onPaginationChangePage">
         <ul class="pages" v-if="computed.total > computed.perPage" slot-scope="{ data, limit, computed, prevButtonEvents, nextButtonEvents, pageButtonEvents }">
-            <!--<li class="" :class="{'disabled': !computed.prevPageUrl}" v-if="computed.prevPageUrl || showDisabled">-->
-            <li>
+            <li class="" :class="{'disabled': !computed.prevPageUrl}" v-if="computed.prevPageUrl || showDisabled">
                 <a class="" href="#" aria-label="Previous" :tabindex="!computed.prevPageUrl && -1" v-on="prevButtonEvents">
                     <slot name="prev-nav">
                         <span aria-hidden="true">&laquo;</span>
@@ -10,11 +9,11 @@
                     </slot>
                 </a>
             </li>
-            <li class="page-item pagination-page-nav" v-for="(page, key) in computed.pageRange" :key="key" :class="{ 'active': page == computed.currentPage }">
-                <a class="page-link" href="#" v-on="pageButtonEvents(page)">{{ page }}</a>
+            <li v-for="(page, key) in computed.pageRange" :key="key" :class="{ 'active': page == computed.currentPage }">
+                <a href="#" v-on="pageButtonEvents(page)">{{ page }}</a>
             </li>
-            <li class="page-item pagination-next-nav" :class="{'disabled': !computed.nextPageUrl}" v-if="computed.nextPageUrl || showDisabled">
-                <a class="page-link" href="#" aria-label="Next" :tabindex="!computed.nextPageUrl && -1" v-on="nextButtonEvents">
+            <li :class="{'disabled': !computed.nextPageUrl}" v-if="computed.nextPageUrl || showDisabled">
+                <a href="#" aria-label="Next" :tabindex="!computed.nextPageUrl && -1" v-on="nextButtonEvents">
                     <slot name="next-nav">
                         <span aria-hidden="true">&raquo;</span>
                         <span class="sr-only">Next</span>
