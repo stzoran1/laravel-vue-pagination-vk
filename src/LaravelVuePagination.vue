472347361ba63c76b6f166efe7f67e8419070c67
Kiza <belteksllc@gmail.com>
2019-02-18 12:06:31 +0100

    Fix

--- src/LaravelVuePagination.vue
+++ src/LaravelVuePagination.vue
@@ -1,36 +1,29 @@
 <template>
     <renderless-laravel-vue-pagination :data="data" :limit="limit" :show-disabled="showDisabled"
                                        v-on:pagination-change-page="onPaginationChangePage">
-        <nav class="pagination" v-if="computed.total > computed.perPage"
-             slot-scope="{ data, limit, computed, prevButtonEvents, nextButtonEvents, pageButtonEvents }">
-            <div class="column">
-                <ul class="pages">
-                    <!--<li class="" :class="{'disabled': !computed.prevPageUrl}" v-if="computed.prevPageUrl || showDisabled">
-                        <a class="" href="#" aria-label="Previous" :tabindex="!computed.prevPageUrl && -1" v-on="prevButtonEvents">
-                            <slot name="prev-nav">
-                                <span aria-hidden="true">&laquo;</span>
-                                <span class="sr-only">Previous</span>
-                            </slot>
-                        </a>
-                    </li>-->
-                    <li v-for="(page, key) in computed.pageRange" :key="key"
-                        :class="{ 'active': page == computed.currentPage }">
-                        <a href="#" v-on="pageButtonEvents(page)">{{ page }}</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="column text-right hidden-xs-down">
-                <!--<li :class="{'disabled': !computed.nextPageUrl}" v-if="computed.nextPageUrl || showDisabled">-->
-                <!--</li>-->
-                <a class="btn btn-outline-secondary btn-sm" href="#" :aria-label="lang('fe.next')"
-                   :tabindex="!computed.nextPageUrl && -1" v-on="nextButtonEvents">{{ lang('fe.next') }}&nbsp;
-                    <slot name="next-nav">
-                        <i class="icon-chevron-right"></i>
-
+        <ul class="pages" v-if="computed.total > computed.perPage"
+            slot-scope="{ data, limit, computed, prevButtonEvents, nextButtonEvents, pageButtonEvents }">
+            <!--<li class="" :class="{'disabled': !computed.prevPageUrl}" v-if="computed.prevPageUrl || showDisabled">
+                <a class="" href="#" aria-label="Previous" :tabindex="!computed.prevPageUrl && -1" v-on="prevButtonEvents">
+                    <slot name="prev-nav">
+                        <span aria-hidden="true">&laquo;</span>
+                        <span class="sr-only">Previous</span>
                     </slot>
                 </a>
-            </div>
-        </nav>
+            </li>-->
+            <li v-for="(page, key) in computed.pageRange" :key="key"
+                :class="{ 'active': page == computed.currentPage }">
+                <a href="#" v-on="pageButtonEvents(page)">{{ page }}</a>
+            </li>
+            <!--<li :class="{'disabled': !computed.nextPageUrl}" v-if="computed.nextPageUrl || showDisabled">
+                <a href="#" aria-label="Next" :tabindex="!computed.nextPageUrl && -1" v-on="nextButtonEvents">
+                    <slot name="next-nav">
+                        <span aria-hidden="true">&raquo;</span>
+                        <span class="sr-only">Next</span>
+                    </slot>
+                </a>
+            </li>-->
+        </ul>
     </renderless-laravel-vue-pagination>
 </template>
 
