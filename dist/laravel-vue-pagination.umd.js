92ee22af38ff7cfbf2eb2aeb9aa4515b2e4bc442
Kiza <belteksllc@gmail.com>
2019-02-18 11:51:30 +0100

    Fix

--- dist/laravel-vue-pagination.umd.js
+++ dist/laravel-vue-pagination.umd.js
@@ -921,7 +921,7 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules//.cache//vue-loader","cacheIdentifier":"cb29a978-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/LaravelVuePagination.vue?vue&type=template&id=4941b921&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules//.cache//vue-loader","cacheIdentifier":"cb29a978-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/LaravelVuePagination.vue?vue&type=template&id=0593909e&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('renderless-laravel-vue-pagination',{attrs:{"data":_vm.data,"limit":_vm.limit,"show-disabled":_vm.showDisabled},on:{"pagination-change-page":_vm.onPaginationChangePage},scopedSlots:_vm._u([{key:"default",fn:function(ref){
 var data = ref.data;
 var limit = ref.limit;
@@ -929,11 +929,11 @@ var computed = ref.computed;
 var prevButtonEvents = ref.prevButtonEvents;
 var nextButtonEvents = ref.nextButtonEvents;
 var pageButtonEvents = ref.pageButtonEvents;
-return _c('div',{staticClass:"column text-right hidden-xs-down"},[_c('a',_vm._g({staticClass:"btn btn-outline-secondary btn-sm",attrs:{"href":"#","aria-label":_vm.lang('fe.next'),"tabindex":!computed.nextPageUrl && -1}},nextButtonEvents),[_vm._v(_vm._s(_vm.lang('fe.next'))+" "),_c('i',{staticClass:"icon-chevron-right"})])])}}])})}
+return _c('nav',{staticClass:"pagination"},[_c('div',{staticClass:"column"},[(computed.total > computed.perPage)?_c('ul',{staticClass:"pages"},_vm._l((computed.pageRange),function(page,key){return _c('li',{key:key,class:{ 'active': page == computed.currentPage }},[_c('a',_vm._g({attrs:{"href":"#"}},pageButtonEvents(page)),[_vm._v(_vm._s(page))])])})):_vm._e()]),_c('div',{staticClass:"column text-right hidden-xs-down"},[_c('a',_vm._g({staticClass:"btn btn-outline-secondary btn-sm",attrs:{"href":"#","aria-label":_vm.lang('fe.next'),"tabindex":!computed.nextPageUrl && -1}},nextButtonEvents),[_vm._v(_vm._s(_vm.lang('fe.next'))+" "),_c('i',{staticClass:"icon-chevron-right"})])])])}}])})}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/LaravelVuePagination.vue?vue&type=template&id=4941b921&
+// CONCATENATED MODULE: ./src/LaravelVuePagination.vue?vue&type=template&id=0593909e&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es6.number.constructor.js
 var es6_number_constructor = __webpack_require__("c5f6");
@@ -1209,6 +1209,8 @@ component.options.__file = "RenderlessLaravelVuePagination.vue"
 /* harmony default export */ var RenderlessLaravelVuePagination = (component.exports);
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/LaravelVuePagination.vue?vue&type=script&lang=js&
 
+//
+//
 //
 //
 //
--- src/LaravelVuePagination.vue
+++ src/LaravelVuePagination.vue
@@ -1,29 +1,31 @@
 <template>
     <renderless-laravel-vue-pagination :data="data" :limit="limit" :show-disabled="showDisabled"
                                        v-on:pagination-change-page="onPaginationChangePage">
-        <div class="column" slot-scope="{ data, limit, computed, prevButtonEvents, nextButtonEvents, pageButtonEvents }">
-            <ul class="pages" v-if="computed.total > computed.perPage">
-                <!--<li class="" :class="{'disabled': !computed.prevPageUrl}" v-if="computed.prevPageUrl || showDisabled">
-                    <a class="" href="#" aria-label="Previous" :tabindex="!computed.prevPageUrl && -1" v-on="prevButtonEvents">
-                        <slot name="prev-nav">
-                            <span aria-hidden="true">&laquo;</span>
-                            <span class="sr-only">Previous</span>
-                        </slot>
-                    </a>
-                </li>-->
-                <li v-for="(page, key) in computed.pageRange" :key="key"
-                    :class="{ 'active': page == computed.currentPage }">
-                    <a href="#" v-on="pageButtonEvents(page)">{{ page }}</a>
-                </li>
-            </ul>
-        </div>
-        <div class="column text-right hidden-xs-down" slot-scope="{ data, limit, computed, prevButtonEvents, nextButtonEvents, pageButtonEvents }">
-            <!--<li :class="{'disabled': !computed.nextPageUrl}" v-if="computed.nextPageUrl || showDisabled">-->
-            <!--</li>-->
-            <a class="btn btn-outline-secondary btn-sm" href="#" :aria-label="lang('fe.next')"
-               :tabindex="!computed.nextPageUrl && -1" v-on="nextButtonEvents">{{ lang('fe.next') }}&nbsp;<i
-                    class="icon-chevron-right"></i></a>
-        </div>
+        <nav class="pagination" slot-scope="{ data, limit, computed, prevButtonEvents, nextButtonEvents, pageButtonEvents }">
+            <div class="column">
+                <ul class="pages" v-if="computed.total > computed.perPage">
+                    <!--<li class="" :class="{'disabled': !computed.prevPageUrl}" v-if="computed.prevPageUrl || showDisabled">
+                        <a class="" href="#" aria-label="Previous" :tabindex="!computed.prevPageUrl && -1" v-on="prevButtonEvents">
+                            <slot name="prev-nav">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </slot>
+                        </a>
+                    </li>-->
+                    <li v-for="(page, key) in computed.pageRange" :key="key"
+                        :class="{ 'active': page == computed.currentPage }">
+                        <a href="#" v-on="pageButtonEvents(page)">{{ page }}</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="column text-right hidden-xs-down">
+                <!--<li :class="{'disabled': !computed.nextPageUrl}" v-if="computed.nextPageUrl || showDisabled">-->
+                <!--</li>-->
+                <a class="btn btn-outline-secondary btn-sm" href="#" :aria-label="lang('fe.next')"
+                   :tabindex="!computed.nextPageUrl && -1" v-on="nextButtonEvents">{{ lang('fe.next') }}&nbsp;<i
+                        class="icon-chevron-right"></i></a>
+            </div>
+        </nav>
     </renderless-laravel-vue-pagination>
 </template>
 
